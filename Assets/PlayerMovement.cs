cb08c4c00a7c5f2d4c467a847cbf2d5ff634fe2f
Qi Rong Kwang <59965128+qrkwang@users.noreply.github.com>
2022-10-29 16:17:40 +0800

    Update playermovement script

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -2,17 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
-{
+public class PlayerMovement : MonoBehaviour{
+
+    public Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("hello world");
+
+        
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        rb.AddForce(0,0,200 * Time.deltaTime);
     }
 }
